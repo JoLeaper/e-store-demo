9dc32507dbdbb75fa1ecb87718a453f851c9c94d
JoLeaper <joleap123@gmail.com>
2020-04-06 14:12:32 -0700

    created starting object

---
+++ cards.js
@@ -0,0 +1,83 @@
+const cards = [
+    {
+        id: 1,
+        Name: 'Dark Magician',
+        Image: './assets/dark-magician.png',
+        Description: 'The ultimate wizard in terms of attack and defense.',
+        Category: 'Monster',
+        Price: 70
+
+    },
+
+    {
+        id: 2,
+        Name: 'Elemental HERO Neos',
+        Image: './assets/neos.png',
+        Description: 'A new Elemental HERO has arrived from Neo-Space! When he initiates a Contact Fusion with a Neo-Spacian his unknown powers are unleashed.',
+        Category: 'Monster',
+        Price: 60
+
+    },
+
+    {
+        id: 3,
+        Name: 'Stardust Dragon',
+        Image: './assets/stardust-dragon.png',
+        Description: `1 Tuner + 1+ non-Tuner monsters
+        When a card or effect is activated that would destroy a card(s) on the field (Quick Effect): You can Tribute this card; negate the activation, and if you do, destroy it. During the End Phase, if this effect was activated this turn (and was not negated): You can Special Summon this card from your GY.`,
+        Category: 'Monster',
+        Price: 50
+
+    },
+
+    {
+        id: 4,
+        Name: 'Number 39: Utopia',
+        Image: './assets/utopia.png',
+        Description: `2 Level 4 monsters
+        When a monster declares an attack: You can detach 1 material from this card; negate the attack. If this card is targeted for an attack, while it has no material: Destroy this card.`,
+        Category: 'Monster',
+        Price: 40
+
+    },
+
+    {
+        id: 5,
+        Name: 'Odd-Eyes Pendulum Dragon',
+        Image: './assets/oddeyes.png',
+        Description: `Pendulum Effect: You can reduce the battle damage you take from an attack involving a Pendulum Monster you control to 0. During your End Phase: You can destroy this card, and if you do, add 1 Pendulum Monster with 1500 or less ATK from your Deck to your hand. You can only use each Pendulum Effect of "Odd-Eyes Pendulum Dragon" once per turn.
+        Monster Effect: If this card battles an opponent's monster, any battle damage this card inflicts to your opponent is doubled.`,
+        Category: 'Monster',
+        Price: 30
+
+    },
+
+    {
+
+        id: 6,
+        Name: 'Decode Talker',
+        Image: './assets/decode-talker.png',
+        Description: `2+ Effect Monsters
+        Gains 500 ATK for each monster it points to. When your opponent activates a card or effect that targets a card(s) you control (Quick Effect): You can Tribute 1 monster this card points to; negate the activation, and if you do, destroy that card.`,
+        Category: 'Monster',
+        Price: 20
+
+    },
+
+    {
+
+        id: 7,
+        Name: 'Sevens Road Magician',
+        Image: './assets/seven-road.png',
+        Description: `[Requirement]: Send the top card from your Deck to the Graveyard.
+        [Effect]: This card gains 300 ATK for each Attribute monster in your graveyard.`,
+        Category: 'Monster',
+        Price: 10
+
+    },
+
+    
+
+];
+
+export default cards;
